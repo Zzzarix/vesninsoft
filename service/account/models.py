0feb4d2446e73112ca9205336049c5fc83016468
Zzzalex <android1vvV@mail.com>
2022-12-21 16:24:21 +1200

    models updated

--- service/account/models.py
+++ service/account/models.py
@@ -1,16 +1,16 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
-
 
 class Company(models.Model):
-    
+    name = models.CharField('Название', null=False, max_length=100)
+    inn = models.BigIntegerField('ИНН', primary_key=True)
+
     def __str__(self) -> str:
-        return f"<User {self.phone}>"
-    
+        return f"<Company {self.name}>"
+
     class Meta:
-        ordering = ['company']
+        ordering = ['name', 'inn']
         verbose_name = 'Организация'
         verbose_name_plural = 'Организации'
 
@@ -19,24 +19,28 @@ class User(AbstractUser):
     USERNAME_FIELD = 'phone'
     REQUIRED_FIELDS = ['phone', 'password']
 
-    phone = models.CharField('Телефон', null=False, primary_key=True, max_length=12)
+    phone = models.CharField('Телефон', null=False,
+                             primary_key=True, max_length=12)
     password = models.CharField('Пароль', null=False, max_length=50)
-    company = models.ForeignKey(Company, 'Организация', null=False, on_delete=models.CASCADE)
+    company = models.ForeignKey(
+        Company, 'Организация', null=False, on_delete=models.CASCADE)
+    full_name = models.CharField('ФИО', max_length=120)
+    inn = models.BigIntegerField('ИНН')
 
     def __str__(self) -> str:
-        return f"<User {self.phone}>"
-    
+        return f"<User {self.phone} {self.inn}>"
+
     class Meta:
-        ordering = ['company']
+        ordering = ['company', 'full_name']
         verbose_name = 'Пользователь'
         verbose_name_plural = 'Пользователи'
 
 
 class Document(models.Model):
-    
+
     def __str__(self) -> str:
-        return f"<User {self.phone}>"
-    
+        return f"<Document {self.phone}>"
+
     class Meta:
         ordering = ['company']
         verbose_name = 'Документ'
@@ -52,7 +56,7 @@ class PayslipRegistry(models.Model):
 
     def __str__(self) -> str:
         return f"<PayslipRegistry {self.company} {self.month}.{self.year}>"
-    
+
     class Meta:
         ordering = ['id', 'company']
         verbose_name = 'Реестр рассчётных листов'
@@ -61,7 +65,8 @@ class PayslipRegistry(models.Model):
 
 class PayslipSheet(models.Model):
     id = models.BigAutoField('Идентификатор', primary_key=True)
-    registry = models.ForeignKey(PayslipRegistry, 'Реестр', on_delete=models.CASCADE)
+    registry = models.ForeignKey(
+        PayslipRegistry, 'Реестр', on_delete=models.CASCADE)
     number = models.IntegerField('Номер листа')
 
     full_name = models.CharField('ФИО', max_length=120)
@@ -72,23 +77,29 @@ class PayslipSheet(models.Model):
     accruals = models.DecimalField('Начисления')
 
     accrual_salary = models.DecimalField('Оклад')
-    accrual_salary_description = models.CharField('Оклад описание начисления', max_length=150)
+    accrual_salary_description = models.CharField(
+        'Оклад описание начисления', max_length=150)
 
-    accrual_regional_coefficient = models.DecimalField('Региональный коеффициент')
-    accrual_regional_coefficient_description = models.CharField('Региональный коеффициент описание начисления', max_length=150)
+    accrual_regional_coefficient = models.DecimalField(
+        'Региональный коеффициент')
+    accrual_regional_coefficient_description = models.CharField(
+        'Региональный коеффициент описание начисления', max_length=150)
 
     accrual_allowances = models.DecimalField('Северные надбавки')
-    accrual_allowances_description = models.CharField('Северные надбавки описание начисления', max_length=150)
+    accrual_allowances_description = models.CharField(
+        'Северные надбавки описание начисления', max_length=150)
 
     holds = models.DecimalField('Удержания')
 
     holds_tax = models.DecimalField('Налог на доходы')
-    holds_tax_description = models.CharField('Налог на доходы описание удержания', max_length=150)
+    holds_tax_description = models.CharField(
+        'Налог на доходы описание удержания', max_length=150)
 
     payments = models.DecimalField('Выплаты')
 
     payments_salary = models.DecimalField('Перечисление зарплаты')
-    payments_salary_description = models.CharField('Перечисление зарплаты описание выплаты', max_length=150)
+    payments_salary_description = models.CharField(
+        'Перечисление зарплаты описание выплаты', max_length=150)
 
     info_days_worked = models.DecimalField('Отработано дней')
     info_hours_worked = models.DecimalField('Отработано часов')
@@ -97,8 +108,8 @@ class PayslipSheet(models.Model):
     info_sum = models.DecimalField('Отработано дней')
 
     def __str__(self) -> str:
-        return f"<PayslipRegistry {self.company} {self.month}.{self.year}>"
-    
+        return f"<PayslipSheet {self.registry.id} {self.number}>"
+
     class Meta:
         ordering = ['id', 'company']
         verbose_name = 'Рассчётный лист'
@@ -115,7 +126,7 @@ class PayslipSheet(models.Model):
 
 #     def __str__(self) -> str:
 #         return f"<PayslipRegistry {self.company} {self.month}.{self.year}>"
-    
+
 #     class Meta:
 #         ordering = ['id', 'full_name']
 #         verbose_name = 'Сотрудник'
