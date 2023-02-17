4a09419a8ae8336bca01a046584a5509c04360c8
Zzzalex <android1vvV@mail.com>
2023-02-17 18:22:17 +1200

    fix

--- service/vesninsoft/settings.py
+++ service/vesninsoft/settings.py
@@ -79,24 +79,24 @@ WSGI_APPLICATION = 'vesninsoft.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'vesninDb',
-#         'USER': 'vesninpg',
-#         'PASSWORD': '7ZQrXgEgJC',
-#         'HOST': 'psql',
-#         'PORT': '5432',
-#         'TIMEOUT': 30
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'vesninDb',
+        'USER': 'vesninpg',
+        'PASSWORD': '7ZQrXgEgJC',
+        'HOST': 'psql',
+        'PORT': '5432',
+        'TIMEOUT': 30
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 # Password validation
