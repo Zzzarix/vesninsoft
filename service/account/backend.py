a1a7cac4d7a1af066630e466b11c0ca408960c64
Zzzalex <android1vvV@mail.com>
2023-02-22 17:11:54 +1200

    fix

--- service/account/backend.py
+++ service/account/backend.py
@@ -7,7 +7,7 @@ def parse_payslip_registry(content: str | bytes):
 
     ENCODING = 'utf-8'  # 'windows-1251'
 
-    content = (content.decode(encoding=ENCODING) if isinstance(content, bytes) else content).replace('<?xml version="1.0" encoding="utf-8" ?>\n', '')
+    content = content.replace('<?xml version="1.0" encoding="utf-8" ?>\n', '')
 
     data: minidom.Document = minidom.parseString(content)
 
